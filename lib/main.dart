97bda3180d033537d860c508c79ce0b3eb712f6c
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2022-07-17 14:10:40 +0200

    add sentry to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_together/widgets/app.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() {
-  runApp(GotogetherApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://26904f548a1149a6b66d2c475b7755bb@o1315229.ingest.sentry.io/6566897';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(GotogetherApp()),
+  );
+
+  // or define SENTRY_DSN via Dart environment variable (--dart-define)
 }
