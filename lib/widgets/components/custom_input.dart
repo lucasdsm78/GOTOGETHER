17405ae7659ffb47deeed6c3e0112236da660a39
gwenael <gwenael.marchetti-waternaux@edu.itescia.fr>
2022-07-20 11:32:17 +0200

    update signup

--- lib/widgets/components/custom_input.dart
+++ lib/widgets/components/custom_input.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 class CustomInput extends StatefulWidget {
   const CustomInput({Key? key, required this.title,
     required this.notValidError, required this.controller,
-    this.type=TextInputType.text }) : super(key: key);
+    this.type = TextInputType.text,
+    this.textColor = Colors.blueAccent,
+  this.isPassword = false,
+  this.border,
+  this.margin,
+  }) : super(key: key);
   final String title;
   final String notValidError;
   final TextEditingController controller;
   final TextInputType type;
+  final Color textColor;
+  final bool isPassword;
+  final InputBorder? border;
+  final EdgeInsetsGeometry? margin;
 
   @override
   _CustomInputState createState() => _CustomInputState();
@@ -29,13 +38,25 @@ class _CustomInputState extends State<CustomInput> {
   Widget build(BuildContext context) {
     return
     Container(
-      margin: const EdgeInsets.only(left: 15.0, right: 20.0, top: 15.0),
+      margin: ( widget.margin == null
+          ?  const EdgeInsets.only(left: 15.0, right: 20.0, top: 15.0)
+          : widget.margin!
+      ),
       child: TextFormField(
+        obscureText: widget.isPassword,
+        cursorColor: widget.textColor,
+        style: TextStyle(
+          color: widget.textColor,
+          decorationColor: widget.textColor
+        ),
         decoration: InputDecoration(
-          fillColor: Colors.blueAccent,
-          border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(50)
+          fillColor: widget.textColor,
+          border: (widget.border == null
+            ? OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(50)
+            )
+            : widget.border!
           ),
           labelText: widget.title,
         ),
